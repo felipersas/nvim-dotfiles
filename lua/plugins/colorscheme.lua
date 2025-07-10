2d2663e1d6aa7f344e64cc5c0f9d4c156843cfd5
Felipe <felipemarques.computacao@gmail.com>
2025-07-10 19:03:16 -0300

    feat: new plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,41 +1,42 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000, -- Força carregar primeiro
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = false,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = true,
-          folds = true,
-        },
-        strikethrough = false,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "soft", -- can be "hard", "soft" or empty string
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-        palette_overrides = {
-          light0_soft = "#EFE0AE",
-          bright_red = "#C14D40",
-          bright_green = "#979A3D",
-          bright_yellow = "#B38E36",
-          bright_blue = "#3D9A78",
-          bright_purple = "#d3869b",
-          bright_aqua = "#7CA66C",
-          bright_orange = "#B37036",
-        },
-      })
-      vim.cmd.colorscheme("gruvbox") -- Aplica o tema
-    end,
-  },
+  { "datsfilipe/min-theme.nvim" },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   priority = 1000, -- Força carregar primeiro
+  --   config = function()
+  --     require("gruvbox").setup({
+  --       terminal_colors = true, -- add neovim terminal colors
+  --       undercurl = true,
+  --       underline = true,
+  --       bold = false,
+  --       italic = {
+  --         strings = true,
+  --         emphasis = true,
+  --         comments = true,
+  --         operators = true,
+  --         folds = true,
+  --       },
+  --       strikethrough = false,
+  --       invert_selection = false,
+  --       invert_signs = false,
+  --       invert_tabline = false,
+  --       inverse = true, -- invert background for search, diffs, statuslines and errors
+  --       contrast = "soft", -- can be "hard", "soft" or empty string
+  --       overrides = {},
+  --       dim_inactive = false,
+  --       transparent_mode = false,
+  --       palette_overrides = {
+  --         light0_soft = "#EFE0AE",
+  --         bright_red = "#C14D40",
+  --         bright_green = "#979A3D",
+  --         bright_yellow = "#B38E36",
+  --         bright_blue = "#3D9A78",
+  --         bright_purple = "#d3869b",
+  --         bright_aqua = "#7CA66C",
+  --         bright_orange = "#B37036",
+  --       },
+  --     })
+  --     vim.cmd.colorscheme("gruvbox") -- Aplica o tema
+  --   end,
+  -- },
 }
